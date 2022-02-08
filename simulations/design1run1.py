4a152d222251b7dc8177e717de7eae93f1f379db
devlaming <devlaming@protonmail.com>
2022-02-08 14:54:29 +0100

    updated simulation design scripts
    improved commenting of how the random seed is set per design, setting, run combination

--- simulations/design1run1.py
+++ simulations/design1run1.py
@@ -18,9 +18,9 @@ dGWASMAF=0.01
 iR=1
 for iSetting in range(1,iSETTINGS):
     #get unique seed for given combination of run, setting, and design
-    #bits 1-4: for iDESIGN between 1 and 15 (=2**4-1)
-    #bits 5-8: for settings between 1 and 15 (=2**(8-4)-1)
-    #bits 9-.: for run between 1 and 16777215 (=2**(32-8)-1)
+    #bits 1-4: for design numbers between 0 and 15 (=2**4-1)
+    #bits 5-8: for setting numbers between 0 and 15 (=2**(8-4)-1)
+    #bits 9-.: for run numbers between 0 and 16777215 (=2**(32-8)-1)
     iThisSeed=(2**0)*iDESIGN+(2**4)*iSetting+(2**8)*iR
     iNGWAS=int(vNGWAS[iSetting-1])
     iN=(2*iC*iNGWAS)+iNPRED
