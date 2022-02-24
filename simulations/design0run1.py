57a50ea8bb2f968e1656468e2b7a5bace1c46c0b
devlaming <devlaming@protonmail.com>
2022-02-24 16:00:21 +0100

    Update simulation scripts
    - changed Design 0 (various N_gwas)
    - changed Design 1 (GN + various N_gwas)
    - added Design 2 (GN + various levels of AM)
    - added Design 3 (GN + various N_holdout)

--- simulations/design0run1.py
+++ simulations/design0run1.py
@@ -1,30 +1,29 @@
 import numpy as np
 from gnames import gnames
-#DESIGN 0: Various GWAS sample sizes, no assortative mating, no genetic nurture
+#DESIGN 0: Various GWAS sample sizes
 iDESIGN=0
-vNGWAS=np.array((1000,2000,5000,10000))
-iNPRED=20000
-iSETTINGS=len(vNGWAS)+1
-dRhoAM=0
+vNGWAS=np.array((1000,2000,4000,8000,16000))
+iNPRED=16000
+iM=5000
+dHsqY=0.25
 dPropGN=0
-#OTHER SPECS
+dRhoAM=0
+dCorrYAM=1
 iC=2
 iT=10
-iM=2000
-dHsqY=0.25
-dCorrYAM=1
 dGWASMAF=0.01
+iSETTINGS=len(vNGWAS)
 #RUN NUMBER
 iR=1
-for iSetting in range(1,iSETTINGS):
+for iSetting in range(iSETTINGS):
     #get unique seed for given combination of run, setting, and design
-    #bits 1-4: for design numbers between 0 and 15 (=2**4-1)
+    #bits 1-4: for design numbers between 0 and 15 (=2**(4-0)-1)
     #bits 5-8: for setting numbers between 0 and 15 (=2**(8-4)-1)
     #bits 9-.: for run numbers between 0 and 16777215 (=2**(32-8)-1)
     iThisSeed=(2**0)*iDESIGN+(2**4)*iSetting+(2**8)*iR
-    iNGWAS=int(vNGWAS[iSetting-1])
+    iNGWAS=int(vNGWAS[iSetting])
     iN=(2*iC*iNGWAS)+iNPRED
-    sName='DESIGN.'+str(iDESIGN)+'.NGWAS.'+str(iNGWAS)+'.RUN.'+str(iR)
+    sName='DESIGN.'+str(iDESIGN)+'.NGWASPOOLED.'+str(2*iNGWAS)+'.RUN.'+str(iR)
     simulator=gnames(iN,iM,iC,dHsqY,dPropGN,dCorrYAM,dRhoAM,iSeed=iThisSeed)
     simulator.Simulate(iT)
     simulator.MakeThreePGIs(sName,iNGWAS,iNPRED,dGWASMAF)
--- simulations/design2run1.py
+++ simulations/design2run1.py
@@ -1,18 +1,18 @@
 import numpy as np
 from gnames import gnames
-#DESIGN 1: Genetic nurture + Various GWAS sample sizes
-iDESIGN=1
-vNGWAS=np.array((1000,2000,4000,8000,16000))
+#DESIGN 2: Genetic nurture + Various levels of assortative mating
+iDESIGN=2
+iNGWAS=16000
 iNPRED=16000
 iM=5000
 dHsqY=0.2
 dPropGN=0.1
-dRhoAM=0
+vRhoAM=np.array((0,0.25,0.5,0.75,1))
 dCorrYAM=1
 iC=2
 iT=10
 dGWASMAF=0.01
-iSETTINGS=len(vNGWAS)
+iSETTINGS=len(vRhoAM)
 #RUN NUMBER
 iR=1
 for iSetting in range(iSETTINGS):
@@ -21,9 +21,9 @@ for iSetting in range(iSETTINGS):
     #bits 5-8: for setting numbers between 0 and 15 (=2**(8-4)-1)
     #bits 9-.: for run numbers between 0 and 16777215 (=2**(32-8)-1)
     iThisSeed=(2**0)*iDESIGN+(2**4)*iSetting+(2**8)*iR
-    iNGWAS=int(vNGWAS[iSetting])
+    dRhoAM=vRhoAM[iSetting]
     iN=(2*iC*iNGWAS)+iNPRED
-    sName='DESIGN.'+str(iDESIGN)+'.NGWASPOOLED.'+str(2*iNGWAS)+'.RUN.'+str(iR)
+    sName='DESIGN.'+str(iDESIGN)+'.RHO_AM.'+'{:.2f}'.format(dRhoAM)+'.RUN.'+str(iR)
     simulator=gnames(iN,iM,iC,dHsqY,dPropGN,dCorrYAM,dRhoAM,iSeed=iThisSeed)
     simulator.Simulate(iT)
     simulator.MakeThreePGIs(sName,iNGWAS,iNPRED,dGWASMAF)
